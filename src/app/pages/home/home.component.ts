94b8a94108e7ef7186a6efe35fc3c19da086dc46
JesusChamiso <jesus.chamiso@hotmail.com>
2024-12-18 17:24:14 -0400

    NgFor para objetos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +9,36 @@ import { Component, signal } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks = signal([
-    'Instalar Angular Cli',
-    'Crear Proyecto',
-    'Crear componentes'
+  tasks = signal<Task[]>([
+    {
+      id:Date.now(),
+      title: 'Instalar Angular Cli',
+      completed: false
+    },
+    {
+      id:Date.now(),
+      title: 'Crar Componente',
+      completed: false
+    },
+    {
+      id:Date.now(),
+      title: 'Crear Servicio',
+      completed: false
+    },
   ])
 
   changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(title:string ) {
+    const newTask = {
+      id:Date.now(),
+      title, 
+      completed: false
+      };
     this.tasks.update((tasks) => [...tasks, newTask]);
   }
 
